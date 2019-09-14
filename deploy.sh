a8853e15745644663394b675dc2def9cb6259b22
Robert den Harink <robert@robhar.com>
2019-09-14 21:00:12 +0200

    deployment

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#bin/#!/usr/bin/env bash
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+docker build -t tacitic/nuts-rs:${TRAVIS_TAG}
+
+docker push tacitic/nuts-rs:${TRAVIS_TAG}'
