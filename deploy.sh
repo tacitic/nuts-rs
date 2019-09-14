b6b632f0f28bb52b1778509c0956957c08b4e397
Robert den Harink <robert@robhar.com>
2019-09-14 21:13:35 +0200

    typos

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker build -t tacitic/nuts-rs:${TRAVIS_TAG}
+docker build -t tacitic/nuts-rs:${TRAVIS_TAG} .
 
-docker push tacitic/nuts-rs:${TRAVIS_TAG}'
+docker push tacitic/nuts-rs:${TRAVIS_TAG}
