afe7a312a72b4aeb2a1ddffcc70a31f7783ef303
Robert den Harink <robert@robhar.com>
2019-09-17 10:03:04 +0200

    deployment tag latest

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,10 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
+# Build container and push
 docker build -t tacitic/nuts-rs:${TRAVIS_TAG} .
-
 docker push tacitic/nuts-rs:${TRAVIS_TAG}
+
+# Tag latests and push
+docker tag tacitic/nuts-rs:${TRAVIS_TAG} tacitic/nuts-rs:latest
+docker push tacitic/nuts-rs:latest
